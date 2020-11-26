d575e8e24ac99fe4bb50d381bf54eb830e4b2688
Ekanshdeep Gupta <ekanshdeep_gupta@yahoo.co.in>
2020-11-26 16:30:11 +0530

    updated todo

--- examples/test.ml
+++
@@ -1,8 +0,0 @@
-let rec string_of_stack stk = let my_string = (Stack.fold string_of_stack_fold "[ " stk) in (String.sub my_string 0 ((String.length my_string) - 2))  ^ " ]"
-(* Take substring to remove last `,` at the end. *)
-
-and
-string_of_stack_fold s elem = s ^ (string_of_stack_elem elem) ^ ", "
-
-and
-string_of_stack_elem elem = string_of_int elem
--- todo.md
+++ todo.md
@@ -1,6 +1,5 @@
 # MiniOO Compiler - todo
 
 -  Implement stateless stack; make everything pure.
-- Handle printing of current flawed stack implementation (printStuff.ml, line 100).
 - Maybe merge stmtlist and stmt into single token, to make ;;; unnecessary on /examples/eg2.oo, line2
 - Add true parallelism.
